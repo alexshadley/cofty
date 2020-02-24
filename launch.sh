e77f352e1a9f0ecaeab42a464ff502c36d1a8e66
Alex Shadley <shadleyalex@gmail.com>
2020-02-24 09:29:30 -0600

    cleans up

--- launch.sh
+++
@@ -1,6 +0,0 @@
-# launches the postgrest container locally
-
-docker run --rm --net=host -p 3000:3000 \
-  -e PGRST_DB_URI="postgres://postgres:Taco2019!@34.70.136.195:5432/cofty" \
-  -e PGRST_DB_ANON_ROLE="postgres" \
-  postgrest/postgrest
